dc65e02c8e4e67e5c3bd1f47a4f949edf360152c
Diogenes <diogenes.henrique.br@gmail.com>
2021-10-13 12:55:27 -0300

    create join article and categories

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    has_one :profile, dependent: :destroy
+    has_many :articles, -> { order 'published_at DESC, title ASC' }, dependent: :destroy
+    has_many :replies, through: :articles, source: :comments
+end
